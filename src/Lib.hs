997f99699d976e23e2131164d97cb6cfc776be9c
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2017-10-25 23:05:11 +0100

    Implement entropy

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- for convenience
+log2 = logBase 2
+
+entropy :: [Double] -> Double
+entropy probs
+  = sum (map entropy' probs)
+  where
+    entropy' :: Double -> Double
+    entropy' 0.0
+      = 0.0
+    entropy' prob
+      = - prob * log2 prob
